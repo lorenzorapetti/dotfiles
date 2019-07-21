a17dde9b483d36931f784ebae47bf7b8d8e25247
Lorenzo Rapetti <lorenzo.rapetti.94@gmail.com>
2019-07-21 21:43:40 +0200

    Changes to i3 config

--- .config/rofi/pulseaudio.sh
+++ .config/rofi/pulseaudio.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 outputs() {
-    OUTPUT=$(pactl list short sinks | cut  -f 2 | rofi -dmenu -p "Output" -mesg "Select prefered output source" )
+    OUTPUT=$(pactl list short sinks | cut -f 2 | rofi -dmenu -p "Output" -mesg "Select prefered output source")
     pacmd set-default-sink "$OUTPUT" >/dev/null 2>&1
 
     for playing in $(pacmd list-sink-inputs | awk '$1 == "index:" {print $2}'); do
@@ -10,7 +10,7 @@ outputs() {
 }
 
 inputs() {
-    INPUT=$(pactl list short sources | cut  -f 2 | grep input | rofi -dmenu -p "Input" -mesg "Select prefered input source" )
+    INPUT=$(pactl list short sources | cut -f 2 | grep input | rofi -dmenu -p "Input" -mesg "Select prefered input source")
     pacmd set-default-source "$INPUT" >/dev/null 2>&1
 
     for recording in $(pacmd list-source-outputs | awk '$1 == "index:" {print $2}'); do
@@ -18,36 +18,14 @@ inputs() {
     done
 }
 
-get_default_sink() {
-    pacmd stat | awk -F": " '/^Default sink name: /{print $2}'
-}
-
-output_volume() {
-     pacmd list-sinks | awk '/^\s+name: /{indefault = $2 == "'"<$(get_default_sink)>"'"}
-             /^\s+muted: / && indefault {muted=$2}
-             /^\s+volume: / && indefault {volume=$5}
-             END { print muted=="no"?volume:"Muted" }'
-}
-
-get_default_source() {
-    pacmd stat | awk -F": " '/^Default source name: /{print $2}'
-}
-
-input_volume() {
-     pacmd list-sources | awk '/^\s+name: /{indefault = $2 == "'"<$(get_default_source)>"'"}
-             /^\s+muted: / && indefault {muted=$2}
-             /^\s+volume: / && indefault {volume=$5}
-             END { print muted=="no"?volume:"Muted" }'
-}
-
 case "$1" in
-    --output)
-        outputs
+--output)
+    outputs
     ;;
-    --input)
-        inputs
+--input)
+    inputs
     ;;
-    *)
-        echo "Wrong argument"
+*)
+    echo "Wrong argument"
     ;;
 esac
