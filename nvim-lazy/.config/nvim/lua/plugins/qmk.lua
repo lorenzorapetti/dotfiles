f10617b0aa75daf0130f36bbe7d9254a08f030b7
Lorenzo Rapetti <lorenzo.rapetti.94@gmail.com>
2024-07-31 00:39:16 +0200

    feat: add qmk

---
+++ nvim-lazy/.config/nvim/lua/plugins/qmk.lua
@@ -0,0 +1,13 @@
+return {
+  "codethread/qmk.nvim",
+  opts = {
+    name = "LAYOUT_split_3x6_3",
+    -- Kinesis Advantage 360
+    layout = {
+      "x x x x x x _ _ _ _ x x x x x x",
+      "x x x x x x _ _ _ _ x x x x x x",
+      "x x x x x x _ _ _ _ x x x x x x",
+      "_ _ _ _ x x x _ x x x _ _ _ _ _",
+    },
+  },
+}
