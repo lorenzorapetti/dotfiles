711d180630e41392a359c6fe2699142557ca5256
Lorenzo Rapetti <lorenzo.rapetti.94@gmail.com>
2024-08-05 21:48:31 +0200

    Add zen mode

---
+++ nvim-lazy/.config/nvim/lua/plugins/zen.lua
@@ -0,0 +1,19 @@
+return {
+  "folke/zen-mode.nvim",
+  opts = {
+    plugins = {
+      kitty = {
+        enabled = true,
+      },
+      alacritty = {
+        enabled = true,
+      },
+      wezterm = {
+        enabled = true,
+      },
+      neovide = {
+        enabled = true,
+      },
+    },
+  },
+}
