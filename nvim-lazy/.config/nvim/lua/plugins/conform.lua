86c97a402af2e0bbb3c6dee2c7c94443e992e2ad
Lorenzo Rapetti <lorenzo.rapetti.94@gmail.com>
2024-08-09 19:30:25 +0200

    feat: add rubocop and standardrb

--- nvim-lazy/.config/nvim/lua/plugins/conform.lua
+++ nvim-lazy/.config/nvim/lua/plugins/conform.lua
@@ -6,11 +6,22 @@ return {
       javascriptreact = { "biome", "prettier", stop_after_first = true },
       typescript = { "biome", "prettier", stop_after_first = true },
       typescriptreact = { "biome", "prettier", stop_after_first = true },
+      ruby = { "rubocop", "standardrb" },
     },
     formatters = {
       biome = {
         condition = function(ctx)
           return vim.fs.find({ "biome.json", "biome.jsonc" }, { path = ctx.filename, upward = true })[1]
+        end,
+      },
+      rubocop = {
+        condition = function()
+          return not vim.g.rubocop_disable and not vim.g.standardrb_enable
+        end,
+      },
+      standardrb = {
+        condition = function()
+          return vim.g.standardrb_enable
         end,
       },
     },
