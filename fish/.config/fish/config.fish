da4aba04f2d93370e5607ad7dbd3764abe6207c6
Lorenzo Rapetti <lorenzo.rapetti.94@gmail.com>
2024-09-03 23:47:55 +0200

    Enable transience for fish shell

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -87,4 +87,10 @@ zoxide init --cmd cd fish | source
 set --export BUN_INSTALL "$HOME/.bun"
 set --export PATH $BUN_INSTALL/bin $PATH
 
+function starship_transient_prompt_func
+    starship module character
+end
+
 starship init fish | source
+
+enable_transience
