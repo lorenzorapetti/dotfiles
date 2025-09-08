8629d083ac19b6827a6aad381912cc7b373a797b
Lorenzo Rapetti <lorenzo.rapetti.94@gmail.com>
2025-09-08 09:56:05 +0200

    Update neovim

--- common/.config/nvim/lua/plugins/lint.lua
+++ common/.config/nvim/lua/plugins/lint.lua
@@ -7,7 +7,23 @@ return {
     linters = {
       yamllint = {
         condition = function(ctx)
-          return vim.fs.find({ ".yamllint", ".yamllint.yml", ".yamllint.yaml" }, { path = ctx.filename, upward = true })
+          return vim.fs.find({ ".yamllint", ".yamllint.yml", ".yamllint.yaml" }, { path = ctx.filename, upward = true })[1]
+        end,
+      },
+      eslint = {
+        condition = function(ctx)
+          return vim.fs.find({
+            ".eslintrc",
+            ".eslintrc.json",
+            ".eslintrc.js",
+            ".eslintrc.cjs",
+            ".eslintrc.mjs",
+            ".eslintrc.yaml",
+            ".eslintrc.yml",
+            "eslint.config.js",
+            "eslint.config.cjs",
+            "eslint.config.mjs",
+          }, { path = ctx.filename, upward = true })[1]
         end,
       },
     },
