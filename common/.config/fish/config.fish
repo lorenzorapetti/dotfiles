18e74727cde7327804ac026dcee07544f514b393
Lorenzo Rapetti <lorenzo.rapetti.94@gmail.com>
2026-06-25 14:00:27 +0200

    Add television fish integration

--- common/.config/fish/config.fish
+++ common/.config/fish/config.fish
@@ -24,8 +24,7 @@ set -gx ATUIN_NOBIND true
 atuin init fish | source
 
 # Bind to ctrl-r in normal and insert mode, add any other bindings you want here too
-bind \cr _atuin_search
-bind -M insert \cr _atuin_search
+tv init fish | source
 
 function zellij_layouts
     tv zellij
