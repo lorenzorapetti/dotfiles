b35aea775131ab27deafe9beac9e01ff250861a4
Lorenzo Rapetti <lorenzo.rapetti.94@gmail.com>
2026-06-30 09:51:53 +0200

    Add theme selection to fish

--- common/.config/fish/config.fish
+++ common/.config/fish/config.fish
@@ -5,12 +5,30 @@ set -gx MANPAGER "bat -plman"
 set -gx SSH_AUTH_SOCK $XDG_RUNTIME_DIR/ssh-agent.socket
 set -gx TODO_DIR $HOME/notes
 
-fish_config theme choose catppuccin-mocha --color-theme=dark
+# Default + export THEME if not already set (inherited env stays as-is, already exported)
+set -q THEME[1]; or set -gx THEME catppuccin-mocha
+
+# Map theme -> color mode. Listed themes pass --color-theme; others omit it.
+switch $THEME
+    case catppuccin-mocha everforest-soft-dark
+        set -f _mode dark
+    case '*'
+        set -f _mode ''
+end
+
+if test -n "$_mode"
+    fish_config theme choose $THEME --color-theme=$_mode
+else
+    fish_config theme choose $THEME
+end
 
 source $HOME/.config/fish/aliases.fish
 source $HOME/.config/fish/utils.fish
 
 fish_add_path -g $HOME/.cargo/bin
+
+set -Ux SUDO_ASKPASS ~/.local/bin/notify-askpass
+alias sudo="sudo -A"
 
 $HOME/.local/bin/mise activate fish | source
 
